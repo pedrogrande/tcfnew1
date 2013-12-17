3baa3a5270f86cb6750196a800e937dc04404941
pedroargento <peter.argent@gmail.com>
2013-12-17 13:54:49 +1100

    add Ninefold offer to home page product pricing

--- config/routes.rb
+++ config/routes.rb
@@ -21,4 +21,12 @@ Tcfnew1::Application.routes.draw do
   get "web-development-and-javascript-courses", to: "course_list#index"
   get "thanks" => "thanks#index"
   get "enrolment_thanks" => "enrolment_thanks#index"
+
+  # redirections
+  get "/courses/web-app-coder-ruby-on-rails", to: redirect('/courses/expert-coder')
+  get "/courses/web-app-coder-angular-and-node", to: redirect('/courses/angular-coder')
+  get "/ruby_on_rails_course_scholarship", to: redirect('/')
+  get "/courses/summer-coder-camp-ruby-on-rails", to: redirect('/courses/summer-coder-camp-rails-angularjs')
+  get "/courses/web-app-coder-pro", to: redirect('/courses/expert-coder')
+
 end
