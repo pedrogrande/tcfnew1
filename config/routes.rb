a3589552385462fb0ae093c7ea319059be3db805
pedroargento <peter.argent@gmail.com>
2013-12-18 22:36:21 +1100

    fixed redirection

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,10 @@
 Tcfnew1::Application.routes.draw do
   root :to => "home#index"
+    get "/courses/web-app-coder-ruby-on-rails", to: redirect('/courses/expert-coder')
+  get "/courses/web-app-coder-angular-and-node", to: redirect('/courses/angular-coder')
+  get "/ruby_on_rails_course_scholarship", to: redirect('/')
+  get "/courses/summer-coder-camp-ruby-on-rails", to: redirect('/courses/summer-coder-camp-rails-angularjs')
+  get "/courses/web-app-coder-pro", to: redirect('/courses/expert-coder')
   devise_for :users, :controllers => {:registrations => "registrations"}
   resources :users
   resources :skills
@@ -23,10 +28,7 @@ Tcfnew1::Application.routes.draw do
   get "enrolment_thanks" => "enrolment_thanks#index"
 
   # redirections
-  get "/courses/web-app-coder-ruby-on-rails", to: redirect('/courses/expert-coder')
-  get "/courses/web-app-coder-angular-and-node", to: redirect('/courses/angular-coder')
-  get "/ruby_on_rails_course_scholarship", to: redirect('/')
-  get "/courses/summer-coder-camp-ruby-on-rails", to: redirect('/courses/summer-coder-camp-rails-angularjs')
-  get "/courses/web-app-coder-pro", to: redirect('/courses/expert-coder')
+  
+
 
 end
