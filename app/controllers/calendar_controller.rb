2feabc286488cc7e05ff08f91a8a4a70dd97ecfb
pedroargento <peter.argent@gmail.com>
2013-12-25 00:51:25 +1100

    calendar start and course form

--- app/controllers/calendar_controller.rb
+++ app/controllers/calendar_controller.rb
@@ -1,4 +1,9 @@
 class CalendarController < ApplicationController
   def index
+  	@this_months_intakes = false
+  	@this_months_events = false
+  	@next_months_intakes = Intake.where('start_date >= ? and start_date < ?', Date.today.end_of_month + 1, (Date.today.end_of_month + 1).end_of_month)
+  	@next_months_events = Event.where('date >= ? and date < ?', Date.today.end_of_month + 1, (Date.today.end_of_month + 1).end_of_month)
+
   end
 end
--- app/helpers/calendar_helper.rb
+++ app/helpers/calendar_helper.rb
@@ -1,10 +1,3 @@
 module CalendarHelper
 
-	def any_courses_starting_this_month?
-		true
-	end
-
-	def any_events_this_month?
-		true
-	end
 end
