8b683362f736bda67d7bce297bb289c589942036
pedroargento <peter.argent@gmail.com>
2013-12-26 20:07:15 +1100

    comment mailer done

---
+++ app/mailers/comment_mailer.rb
@@ -0,0 +1,13 @@
+class CommentMailer < ActionMailer::Base
+  default from: "pete@thecoderfactory.com"
+
+  # Subject can be set in your I18n file at config/locales/en.yml
+  # with the following lookup:
+  #
+  #   en.comment_mailer.received.subject
+  #
+  def received(comment)
+    @comment = comment
+    mail(to: "info@thecoderfactory.com", subject: "Comment on The Coder Factory blog")
+  end
+end
