fc5b9ec32eae553fddeaa8b449ed429df1196f40
pedroargento <peter.argent@gmail.com>
2014-01-18 09:06:16 +1100

    update sitemap and remove param links from home page

--- config/sitemap.rb
+++ config/sitemap.rb
@@ -1,5 +1,5 @@
 # Set the host name for URL creation
-SitemapGenerator::Sitemap.default_host = "http://thecoderfactory.com"
+SitemapGenerator::Sitemap.default_host = "https://thecoderfactory.com"
 
 SitemapGenerator::Sitemap.create do
   # Put links creation logic here.
